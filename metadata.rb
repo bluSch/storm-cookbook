234604a9ad0780dd87e21009d5d5547231de6b17
B Schlueter <bs@bschlueter.com>
2013-07-29 11:03:27 -0700

    initial build out of repo

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'Storm'
+maintainer       'bluSch'
+maintainer_email 'b@bluSch.net'
+license          IO.read(File.join(File.dirname(__FILE__), 'LICENSE'))
+description      'Installs Twitter\'s Storm distributed computation system'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+supports         'centos'
